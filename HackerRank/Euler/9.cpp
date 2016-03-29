feba8439e85806deb6800d2dcc7edccd7539dd8e
abhishek0007 <hellocoolabhishek@gmail.com>
2016-03-30 02:19:18 +0530

    9th

---
+++ HackerRank/Euler/9.cpp
@@ -0,0 +1,25 @@
+//
+// Created by a on 30/3/16.
+//
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    int n, k;
+    string s;
+    cin >> t;
+    while (t--) {
+        int n, k = 0, max = -1;
+        cin >> n;
+        for (int a = 0; a <= n; a++)
+            for (int b = 0; b < a; b++)
+                for (int c = 0; c < b; c++)
+                    if ((a + b + c) == n && (c * c + b * b) == (a * a)) if (max < a * b * c)
+                        max = a * b * c;
+        cout << max << endl;
+    }
+    return 0;
+}
